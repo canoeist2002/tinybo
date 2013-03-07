e414da8f4da658ffcae82c3a6279937f76ad2570
canoeist2002@gmail.com <canoeist2002@gmail.com>
2013-03-07 19:03:38 +0800

    Finish user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,22 @@
+# -*- encoding : utf-8 -*-
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 20)
+    authorize! :index, @users, :message => '没有足够的权限访问。'
   end
 
   def show
     @user = User.find(params[:id])
+
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "用户删除成功"
+    redirect_to users_path
   end
 end
